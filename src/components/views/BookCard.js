1a6cca812273c43d946352bfc67493f2ba93d0f2
Xevoxe <xevoxe@gmail.com>
2019-02-28 17:17:17 -0600

    ResultsView layout complete.

--- src/components/views/BookCard.js
+++ src/components/views/BookCard.js
@@ -7,19 +7,20 @@ const styles = theme => ({
         display: 'flex',
         flexFlow: 'wrap',
         flexDirection: 'column',
-        maxWidth: '500px',
-        minWidth: '300px',
+        minWidth: '375px',
+        maxWidth: '375px',
         marginTop:'50px',
         overflow: 'visible',
         padding:'0px 20px',
+        alignItems: 'flex-start',
         [theme.breakpoints.up('500')]: {
             flexDirection: 'row',
         },
     },
     bookCover: {
-        width: '175px',
-        margin: 'auto',
+        width: '150px',
         transform: 'translate(0px, -50px)',
+        
     },
     contentDetails: {
         transform:'translate(0px, -25px)',
@@ -37,28 +38,31 @@ const styles = theme => ({
 
 const BookCard = (props) => {
 
-    const {classes} = props;
+    const {classes, image, authors, publisher, title, subTitle, link} = props;
  
     return ( 
             <Card className={classes.container}> 
                 <Paper className={classes.bookCover} elevation={10}>
                     <CardMedia
                         component="img"
-                        image="https://books.google.com/books?id=zyTCAlFPjgYC&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api"
+                        image={image}
                         title="Hunger Games"
                     />
                 </Paper>
                 <div className={classes.contentDetails}>
+                    <Typography  fontWeight={600} component="h2" variant="h6" color="textSecondary">
+                       {title}
+                    </Typography>
                     <Typography  fontWeight={600} component="h2" variant="h6" color="textSecondary" gutterBottom>
-                        Harry Potter and the Cursed Child- Parts One and Two(Special Rehearsal Edition)
+                       {subTitle}
                     </Typography>
                     <Typography component="h3" variant="subtitle1" color="textSecondary">
-                        By: No authors found
+                        By: {authors}
                     </Typography>
                     <Typography component="h4" variant="subtitle1" color="textSecondary" gutterBottom>
-                        Published By: Pottermore Publishing
+                        Published By: {publisher}
                     </Typography>
-                    <Button className={classes.bookButton} variant="contained" color="primary">
+                    <Button className={classes.bookButton} variant="contained" color="primary" href={link}>
                         See this Book
                     </Button>
                 </div>
--- src/components/views/ResultsView.js
+++ src/components/views/ResultsView.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import FetchSearch from '../containers/FetchSearch';
+import BookCard from '../views/BookCard';
+import {withStyles, FormHelperText} from '@material-ui/core';
+
+const styles = theme => ({
+    resultsContainer: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-evenly",
+        margin: '0 10%',
+       
+    },
+    bookCard: {
+        display:'flex',
+        margin: '15px 0px',
+    }
+});
 
 const ResultsView = (props) =>
 {
-    console.log("ResultsView");
-    console.log(props);
+    const {classes, volumes} = props;
+    
     return (
-        <div>Search Results go here!</div>
+        <div className={classes.resultsContainer}>
+            {volumes.map((book)=>{
+                return (
+                    <div className={classes.bookCard}>
+                    <BookCard
+                        image={book.volumeInfo.imageLinks.smallThumbnail}
+                        authors={book.volumeInfo.authors}
+                        publisher={book.volumeInfo.publisher}
+                        title={book.volumeInfo.title}
+                        subTitle={book.volumeInfo.subtitle}
+                        link={book.volumeInfo.infoLink}
+                    />
+                    </div>
+                )
+            })
+            }
+        </div>
     )
 }
-
-const Results = FetchSearch(ResultsView)
+const withStylesResults = withStyles(styles)(ResultsView);
+const Results = FetchSearch(withStylesResults)
 
 export default Results;
